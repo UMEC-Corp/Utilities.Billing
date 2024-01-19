e3cb5afad8f4bf00074bde119ea193b9d47aacb9
Dmitry Nadolin <nadolin@yandex.ru>
2024-01-19 12:53:57 +0200

    UMEC-36

--- src/Utilities.Billing.Tests/Data/MigrationTests.cs
+++ src/Utilities.Billing.Tests/Data/MigrationTests.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Moq;
 using Utilities.Billing.Data;
-using Utilities.Common.Data;
 
 namespace Utilities.Billing.Tests.Data;
 
 /// <remarks>
-/// In memory datacontext doesn't support migrations so using sqlite
+/// In memory DbContext doesn't support migrations so using sqlite
 /// </remarks>>
 public class MigrationTests
 {
@@ -32,28 +27,29 @@ public class MigrationTests
     [Test]
     public async Task DbContextMigratorShouldMigrateEmptyDb()
     {
-        var serviceCollection = new ServiceCollection();
+        Assert.Inconclusive("This test should be moved to the Common repo");
+        //var serviceCollection = new ServiceCollection();
 
-        serviceCollection.AddDbContext<BillingDbContext>(o =>
-        {
-            o.UseSqlite("Data Source=:memory:");
-        }, ServiceLifetime.Singleton);
+        //serviceCollection.AddDbContext<BillingDbContext>(o =>
+        //{
+        //    o.UseSqlite("Data Source=:memory:");
+        //}, ServiceLifetime.Singleton);
 
 
-        var dbContext = new BillingDbContext(serviceCollection.BuildServiceProvider()
-            .GetRequiredService<DbContextOptions<BillingDbContext>>());
+        //var dbContext = new BillingDbContext(serviceCollection.BuildServiceProvider()
+        //    .GetRequiredService<DbContextOptions<BillingDbContext>>());
 
-        var migrator = new DbContextMigrator<BillingDbContext>(serviceCollection.BuildServiceProvider(), Mock.Of<IHost>(),
-            Mock.Of<ILogger<DbContextMigrator<BillingDbContext>>>());
+        //var migrator = new DbContextMigrator<BillingDbContext>(serviceCollection.BuildServiceProvider(), Mock.Of<IHost>(),
+        //    Mock.Of<ILogger<DbContextMigrator<BillingDbContext>>>());
 
-        await dbContext.Database.OpenConnectionAsync();
+        //await dbContext.Database.OpenConnectionAsync();
 
-        var appliedMigrations =
-            await migrator.Migrate(dbContext, CancellationToken.None);
+        //var appliedMigrations =
+        //    await migrator.Migrate(dbContext, CancellationToken.None);
 
-        Assert.That(appliedMigrations, Is.Not.Empty);
+        //Assert.That(appliedMigrations, Is.Not.Empty);
 
-        await dbContext.Database.CloseConnectionAsync();
+        //await dbContext.Database.CloseConnectionAsync();
 
     }
 }
