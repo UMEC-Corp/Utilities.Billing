28048ee58776b05937d32d793a0dadd86f72a350
klogo_hopper <sandro-248@mail.ru>
2024-09-05 17:07:54 +0300

    UMEC-483 recreate migrations

--- src/Utilities.Billing.Data/Migrations/20240905091454_Initial.cs
+++ src/Utilities.Billing.Data/Migrations/20240905091454_Initial.cs
@@ -13,12 +13,33 @@ namespace Utilities.Billing.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
+                name: "payments",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    token_amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency_amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    date_to = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    transaction = table.Column<string>(type: "text", nullable: true),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_payments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "tenants",
                 columns: table => new
                 {
                     id = table.Column<Guid>(type: "uuid", nullable: false),
                     name = table.Column<string>(type: "text", nullable: false),
                     currency = table.Column<string>(type: "text", nullable: false),
+                    wallet = table.Column<string>(type: "text", nullable: false),
                     created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
@@ -56,9 +77,10 @@ namespace Utilities.Billing.Data.Migrations
                 {
                     id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    tenant_id = table.Column<Guid>(type: "uuid", nullable: false),
                     name = table.Column<string>(type: "text", nullable: false),
                     token = table.Column<string>(type: "text", nullable: false),
-                    tenant_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    wallet = table.Column<string>(type: "text", nullable: false),
                     created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
@@ -74,13 +96,63 @@ namespace Utilities.Billing.Data.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "accounts",
+                name: "assets",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tenant_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    issuer = table.Column<string>(type: "text", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_assets", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_assets_tenants_tenant_id",
+                        column: x => x.tenant_id,
+                        principalTable: "tenants",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "exchange_rates",
                 columns: table => new
                 {
                     id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    account_holder_id = table.Column<long>(type: "bigint", nullable: false),
                     account_type_id = table.Column<long>(type: "bigint", nullable: false),
+                    effective = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    sell_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_exchange_rates", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_exchange_rates_account_types_account_type_id",
+                        column: x => x.account_type_id,
+                        principalTable: "account_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tenant_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    asset_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    wallet = table.Column<string>(type: "text", nullable: false),
+                    controller_serial = table.Column<string>(type: "text", nullable: false),
+                    meter_number = table.Column<string>(type: "text", nullable: false),
+                    account_holder_id = table.Column<long>(type: "bigint", nullable: true),
+                    account_type_id = table.Column<long>(type: "bigint", nullable: true),
                     created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
@@ -91,12 +163,44 @@ namespace Utilities.Billing.Data.Migrations
                         name: "fk_accounts_account_holder_account_holder_id",
                         column: x => x.account_holder_id,
                         principalTable: "account_holder",
-                        principalColumn: "id",
-                        onDelete: ReferentialAction.Cascade);
+                        principalColumn: "id");
                     table.ForeignKey(
                         name: "fk_accounts_account_types_account_type_id",
                         column: x => x.account_type_id,
                         principalTable: "account_types",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_accounts_assets_asset_id",
+                        column: x => x.asset_id,
+                        principalTable: "assets",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_accounts_tenants_tenant_id",
+                        column: x => x.tenant_id,
+                        principalTable: "tenants",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "equipment_models",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    asset_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_equipment_models", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_equipment_models_assets_asset_id",
+                        column: x => x.asset_id,
+                        principalTable: "assets",
                         principalColumn: "id",
                         onDelete: ReferentialAction.Cascade);
                 });
@@ -107,10 +211,10 @@ namespace Utilities.Billing.Data.Migrations
                 {
                     id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    account_id = table.Column<long>(type: "bigint", nullable: false),
-                    amount_total = table.Column<decimal>(type: "numeric", nullable: false),
-                    amount_payed = table.Column<decimal>(type: "numeric", nullable: false),
-                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    account_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    xdr = table.Column<string>(type: "text", nullable: false),
+                    payer_wallet = table.Column<string>(type: "text", nullable: false),
                     created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
@@ -119,31 +223,6 @@ namespace Utilities.Billing.Data.Migrations
                     table.PrimaryKey("pk_invoices", x => x.id);
                     table.ForeignKey(
                         name: "fk_invoices_accounts_account_id",
-                        column: x => x.account_id,
-                        principalTable: "accounts",
-                        principalColumn: "id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "payments",
-                columns: table => new
-                {
-                    id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    account_id = table.Column<long>(type: "bigint", nullable: false),
-                    amount = table.Column<decimal>(type: "numeric", nullable: false),
-                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    status = table.Column<int>(type: "integer", nullable: false),
-                    transaction = table.Column<string>(type: "text", nullable: true),
-                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    deleted = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("pk_payments", x => x.id);
-                    table.ForeignKey(
-                        name: "fk_payments_accounts_account_id",
                         column: x => x.account_id,
                         principalTable: "accounts",
                         principalColumn: "id",
@@ -171,19 +250,45 @@ namespace Utilities.Billing.Data.Migrations
                 column: "account_type_id");
 
             migrationBuilder.CreateIndex(
-                name: "ix_invoices_account_id",
-                table: "invoices",
-                column: "account_id");
+                name: "ix_accounts_asset_id",
+                table: "accounts",
+                column: "asset_id");
 
             migrationBuilder.CreateIndex(
-                name: "ix_payments_account_id",
-                table: "payments",
+                name: "ix_accounts_tenant_id",
+                table: "accounts",
+                column: "tenant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_assets_tenant_id",
+                table: "assets",
+                column: "tenant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_equipment_models_asset_id",
+                table: "equipment_models",
+                column: "asset_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_exchange_rates_account_type_id",
+                table: "exchange_rates",
+                column: "account_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invoices_account_id",
+                table: "invoices",
                 column: "account_id");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "equipment_models");
+
+            migrationBuilder.DropTable(
+                name: "exchange_rates");
+
             migrationBuilder.DropTable(
                 name: "invoices");
 
@@ -198,6 +303,9 @@ namespace Utilities.Billing.Data.Migrations
 
             migrationBuilder.DropTable(
                 name: "account_types");
+
+            migrationBuilder.DropTable(
+                name: "assets");
 
             migrationBuilder.DropTable(
                 name: "tenants");
