52100e02b98e40673cdbc764dc645f7221806335
klogo_hopper <sandro-248@mail.ru>
2024-09-05 17:07:19 +0300

    UMEC-483

--- src/Utilities.Billing.Tests/StellarWallets/WalletsManagementTests.cs
+++ src/Utilities.Billing.Tests/StellarWallets/WalletsManagementTests.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using StellarDotnetSdk.Accounts;
-using Utilities.Billing.Contracts;
-using Utilities.Billing.StellarWallets;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.DependencyInjection;
+//using StellarDotnetSdk.Accounts;
+//using Utilities.Billing.Contracts;
+//using Utilities.Billing.StellarWallets;
 
-namespace Utilities.Billing.Tests.StellarWallets;
-class WalletsManagementTests
-{
-    private const string HorizonUrl = "https://horizon-testnet.stellar.org";
+//namespace Utilities.Billing.Tests.StellarWallets;
+//class WalletsManagementTests
+//{
+//    private const string HorizonUrl = "https://horizon-testnet.stellar.org";
 
-    [Test]
-    public async Task Should_Create_Wallet()
-    {
-        var services = new ServiceCollection();
-        services.UseStellarWallets(new StellarWalletsSettings
-        {
-            HorizonUrl = HorizonUrl,
-            SecretSeed = KeyPair.Random().SecretSeed
-        });
+//    [Test]
+//    public async Task Should_Create_Wallet()
+//    {
+//        var services = new ServiceCollection();
+//        services.UseStellarWallets(new StellarWalletsSettings
+//        {
+//            HorizonUrl = HorizonUrl,
+//            SecretSeed = KeyPair.Random().SecretSeed
+//        });
 
-        var provider = services.BuildServiceProvider();
-        var paymentSystem = provider.GetRequiredService<IPaymentSystem>();
+//        var provider = services.BuildServiceProvider();
+//        var paymentSystem = provider.GetRequiredService<IPaymentSystem>();
 
-        var wallet =
-            await paymentSystem.CreateWalletAsync(new CreateWalletCommand(TenantId: Guid.NewGuid(), Token: "TEST"));
+//        var wallet =
+//            await paymentSystem.CreateWalletAsync(new CreateWalletCommand(TenantId: Guid.NewGuid(), Token: "TEST"));
 
-        Assert.That(wallet, Is.Not.Empty);
-    }
+//        Assert.That(wallet, Is.Not.Empty);
+//    }
 
-    [Test]
-    public async Task Should_Create_Asset()
-    {
-        Assert.Inconclusive();
-    }
-}
+//    [Test]
+//    public async Task Should_Create_Asset()
+//    {
+//        Assert.Inconclusive();
+//    }
+//}
--- src/Utilities.Billing.Tests/TenantGrain/PaymentTests.cs
+++ src/Utilities.Billing.Tests/TenantGrain/PaymentTests.cs
@@ -1,94 +1,94 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Utilities.Billing.Contracts;
-using Utilities.Billing.Data.Entities;
+﻿//using Microsoft.EntityFrameworkCore.ChangeTracking;
+//using Utilities.Billing.Contracts;
+//using Utilities.Billing.Data.Entities;
 
-namespace Utilities.Billing.Tests.TenantGrain;
+//namespace Utilities.Billing.Tests.TenantGrain;
 
-public class PaymentTests : GrainsFixtureBase<PaymentsSiloConfigurator>
-{
-    private Guid _tenantId;
-    private ITenantGrain _tenant;
-    private EntityEntry<Account> _account;
-    private EntityEntry<AccountType> _accountType;
+//public class PaymentTests : GrainsFixtureBase<PaymentsSiloConfigurator>
+//{
+//    private Guid _tenantId;
+//    private ITenantGrain _tenant;
+//    private EntityEntry<Account> _account;
+//    private EntityEntry<AccountType> _accountType;
 
-    [SetUp]
-    public async Task SetUp()
-    {
-        _tenantId = Guid.NewGuid();
+//    [SetUp]
+//    public async Task SetUp()
+//    {
+//        _tenantId = Guid.NewGuid();
 
-        _tenant = Cluster.GrainFactory.GetGrain<ITenantGrain>(_tenantId);
+//        _tenant = Cluster.GrainFactory.GetGrain<ITenantGrain>(_tenantId);
 
-        await SiloConfigurator.DbContext.Tenants.AddAsync(new Tenant
-        {
-            Id = _tenantId,
-            Currency = Guid.NewGuid().ToString(),
-            Wallet = Guid.NewGuid().ToString(),
-            Name = $"tenant-{_tenantId}"
-        });
+//        await SiloConfigurator.DbContext.Tenants.AddAsync(new Tenant
+//        {
+//            Id = _tenantId,
+//            Currency = Guid.NewGuid().ToString(),
+//            Wallet = Guid.NewGuid().ToString(),
+//            Name = $"tenant-{_tenantId}"
+//        });
 
-        _accountType = await SiloConfigurator.DbContext.AccountTypes.AddAsync(new AccountType
-        {
-            Token = Guid.NewGuid().ToString(),
-            Name = Guid.NewGuid().ToString(),
-            TenantId = _tenantId,
-            Wallet = Guid.NewGuid().ToString(),
-        });
+//        _accountType = await SiloConfigurator.DbContext.AccountTypes.AddAsync(new AccountType
+//        {
+//            Token = Guid.NewGuid().ToString(),
+//            Name = Guid.NewGuid().ToString(),
+//            TenantId = _tenantId,
+//            Wallet = Guid.NewGuid().ToString(),
+//        });
 
-        var accountHolder = await SiloConfigurator.DbContext.AccountHolder.AddAsync(new AccountHolder
-        {
-            Wallet = Guid.NewGuid().ToString(),
-            TenantId = _tenantId,
-        });
+//        var accountHolder = await SiloConfigurator.DbContext.AccountHolder.AddAsync(new AccountHolder
+//        {
+//            Wallet = Guid.NewGuid().ToString(),
+//            TenantId = _tenantId,
+//        });
 
-        _account = await SiloConfigurator.DbContext.Accounts.AddAsync(new Account
-        {
-            AccountHolder = accountHolder.Entity,
-            AccountType = _accountType.Entity,
-            Wallet = Guid.NewGuid().ToString(),
-        });
+//        _account = await SiloConfigurator.DbContext.Accounts.AddAsync(new Account
+//        {
+//            AccountHolder = accountHolder.Entity,
+//            AccountType = _accountType.Entity,
+//            Wallet = Guid.NewGuid().ToString(),
+//        });
 
-        await SiloConfigurator.DbContext.SaveChangesAsync();
-    }
+//        await SiloConfigurator.DbContext.SaveChangesAsync();
+//    }
 
-    [Test]
-    public async Task Tenant_Should_Create_One_Payment_For_Single_Invoice()
-    {
-        var amount = 100m;
+//    [Test]
+//    public async Task Tenant_Should_Create_One_Payment_For_Single_Invoice()
+//    {
+//        var amount = 100m;
 
-        SiloConfigurator.DbContext.ExchangeRates.AddAsync(new ExchangeRate
-        {
-            AccountType = _accountType.Entity,
-            Effective = DateTime.UtcNow.AddDays(-1),
-            SellPrice = 0.5m,
-        });
+//        SiloConfigurator.DbContext.ExchangeRates.AddAsync(new ExchangeRate
+//        {
+//            AccountType = _accountType.Entity,
+//            Effective = DateTime.UtcNow.AddDays(-1),
+//            SellPrice = 0.5m,
+//        });
 
-        var invoice = await SiloConfigurator.DbContext.Invoices.AddAsync(new Invoice
-        {
-            Account = _account.Entity,
-            AmountTotal = amount,
-            AmountPayed = amount / 3,
-            Date = DateTime.Now,
-        });
+//        var invoice = await SiloConfigurator.DbContext.Invoices.AddAsync(new Invoice
+//        {
+//            Account = _account.Entity,
+//            AmountTotal = amount,
+//            AmountPayed = amount / 3,
+//            Date = DateTime.Now,
+//        });
 
-        await SiloConfigurator.DbContext.SaveChangesAsync();
+//        await SiloConfigurator.DbContext.SaveChangesAsync();
 
-        var command = new AddPaymentsForInvoicesCommand
-        {
-            InvoiceIds = { invoice.Entity.Id }
-        };
+//        var command = new AddPaymentsForInvoicesCommand
+//        {
+//            InvoiceIds = { invoice.Entity.Id }
+//        };
 
-        var addPaymentsReply = await _tenant.AddPaymentsForInvoicesAsync(command);
+//        var addPaymentsReply = await _tenant.AddPaymentsForInvoicesAsync(command);
 
-        var payment = await SiloConfigurator.DbContext.Payments.FindAsync(addPaymentsReply.PaymentIds.Single());
+//        var payment = await SiloConfigurator.DbContext.Payments.FindAsync(addPaymentsReply.PaymentIds.Single());
 
-        Assert.That(payment, Is.Not.Null);
-        Assert.That(payment.TokenAmount, Is.EqualTo(2m * amount / 3m));
-        Assert.That(payment.CurrencyAmount, Is.EqualTo(2m * amount / 3m * 0.5m));
-        Assert.That(payment.AccountId, Is.EqualTo(_account.Entity.Id));
-    }
-}
+//        Assert.That(payment, Is.Not.Null);
+//        Assert.That(payment.TokenAmount, Is.EqualTo(2m * amount / 3m));
+//        Assert.That(payment.CurrencyAmount, Is.EqualTo(2m * amount / 3m * 0.5m));
+//        Assert.That(payment.AccountId, Is.EqualTo(_account.Entity.Id));
+//    }
+//}
 
-public class PaymentsSiloConfigurator : SiloConfigurator
-{
+//public class PaymentsSiloConfigurator : SiloConfigurator
+//{
 
-}
+//}
